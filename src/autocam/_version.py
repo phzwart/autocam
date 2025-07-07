4a21a3beb7d3cfb84ec14fb7ae178160ffb76280
phzwart <PHZwart@lbl.gov>
2025-07-07 14:52:01 -0700

    Complete dependency refresh: update all packages to latest versions and resolve security vulnerabilities

--- src/autocam/_version.py
+++ src/autocam/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev33+gc90e624.d20250707'
-__version_tuple__ = version_tuple = (0, 1, 'dev33', 'gc90e624.d20250707')
+__version__ = version = '0.1.dev34+g2654abd.d20250707'
+__version_tuple__ = version_tuple = (0, 1, 'dev34', 'g2654abd.d20250707')
